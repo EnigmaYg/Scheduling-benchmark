a2ed9b215c0b7f96a302e9f5097549c28ff9a654
名称 <>
2025-03-30 12:46:37 +0800

    update

--- code/parse_concrete_procedural.py
+++ code/parse_concrete_procedural.py
@@ -9,8 +9,10 @@ import json
 # with open('../data/WikiHow_ProceduralQ.json', 'r') as f:
 #     proceduralQ = json.load(f)
 #
-# with open('../data/WikiHow_question2id.json', 'r') as f:
-#     id2question = json.load(f)
+with open('../data/WikiHow_question2id.json', 'r') as f:
+    question2id = json.load(f)
+with open('../data/WikiHow_procedural_question.json', 'r') as f:
+    procedural_question = json.load(f)
 #
 # save = {}
 # cnt = 0
@@ -31,24 +33,34 @@ import json
 #             cnt += 1
 # with open('../data/WikiHow_procedural_question.json', 'w') as f:
 #     json.dump(save, f, indent=4)
-
 with open('../data/WikiHow_Concrete.json', 'r') as f:
-    concrete = json.load(f)
+    concrete1 = json.load(f)
 
 with open('../data/WikiHow_Concrete2.json', 'r') as f:
     concrete2 = json.load(f)
 
+with open('../data/WikiHow_Concrete3.json', 'r') as f:
+    concrete3 = json.load(f)
+
 cnt_yes = 0
 cnt_no = 0
+concrete = {}
+for key, value in concrete1.items():
+    concrete[key] = value
+
+for key, value in concrete2.items():
+    concrete[key] = value
+
+for key, value in concrete3.items():
+    if key in concrete:
+        continue
+    concrete[key] = value
+
+save = {}
 for key, value in concrete.items():
     if value == 'Yes':
-        cnt_yes += 1
-    else:
-        cnt_no += 1
-for key, value in concrete2.items():
-    if value == 'Yes':
-        cnt_yes += 1
-    else:
-        cnt_no += 1
-print(cnt_yes)
-print(cnt_no+cnt_yes)
+        id = question2id[key]
+        save[id] = procedural_question[id]
+print(len(save))
+with open('../data/WikiHow_procedural_concrete_question.json', 'w') as f:
+    json.dump(save, f, indent=4)
