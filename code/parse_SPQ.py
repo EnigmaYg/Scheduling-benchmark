db5162996c5607f9a05d506b03a7629b4b82506b
EnigmaYg <idlepggyy@gmail.com>
2025-04-08 23:48:14 +0800

    update

--- code/parse_SPQ.py
+++ code/parse_SPQ.py
@@ -1,37 +1,24 @@
 import json
 
-# with open('../data/WikiHow_SPQ_5873.json', 'r') as f:
-#     SPQ = json.load(f)
-#
-# with open('../data/WikiHow_SPQ_11746.json', 'r') as f:
-#     SPQ2 = json.load(f)
-#
-# cnt_yes = 0
-# cnt_no = 0
-# cnt = 0
-# for key, value in SPQ.items():
-#     if value == 'yes':
-#         cnt_yes += 1
-#     elif value == 'no':
-#         cnt_no += 1
-#     else:
-#         print(value)
-#     cnt += 1
-# print(cnt)
-# print(cnt_yes)
-# print(cnt_no)
-#
-# for key, value in SPQ2.items():
-#     if value == 'yes':
-#         cnt_yes += 1
-#     elif value == 'no':
-#         cnt_no += 1
-#     else:
-#         print(value)
-#     cnt += 1
-# print(cnt)
-# print(cnt_yes)
-# print(cnt_no)
+
+with open('../data/WikiHow_SPQ.json', 'r') as f:
+    SPQ = json.load(f)
+
+cnt_yes = 0
+cnt_no = 0
+cnt = 0
+for key, value in SPQ.items():
+    if value == 'yes':
+        cnt_yes += 1
+    elif value == 'no':
+        cnt_no += 1
+    else:
+        print(value)
+    cnt += 1
+print(cnt)
+print(cnt_yes)
+print(cnt_no)
+
 
 with open('../data/WikiHow_humanity.json', 'r') as f:
     human = json.load(f)
--- code/parse_settings.py
+++ code/parse_settings.py
@@ -15,8 +15,11 @@ for key, value in setting2.items():
     settings[key] = value
 
 save = {}
+cnt = 0
+all_cnt = 0
 for key, value in settings.items():
     tmp_dic = {}
+    all_flag = 0
     for v_key, v_value in value.items():
         value_list = v_value.split('\n')
         flag = 0
@@ -26,13 +29,20 @@ for key, value in settings.items():
                     tmp_dic[v_key] = 'None'
                     flag = 1
                 else:
+                    all_flag = 1
                     tmp_dic[v_key] = [line.split('[Tools:]')[1].strip(' ')]
             if flag == 1:
                 break
             elif '[Scenario Description:]' in line:
+                all_flag = 1
                 tmp_dic[v_key].append(line.split('[Scenario Description:]')[1].strip(' '))
+    if all_flag == 0:
+        cnt += 1
+    all_cnt += len(tmp_dic)
     save[key] = tmp_dic
 print(len(save))
+print(cnt)
+print(all_cnt)
 
 # with open('../data/WikiHow_setting.json', 'w') as f:
 #     json.dump(save, f, indent=4)
