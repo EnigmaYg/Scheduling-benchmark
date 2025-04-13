d1c6ba50f035fb252fdfb65a6e9b792b2f302785
EnigmaYg <idlepggyy@gmail.com>
2025-04-13 12:39:18 +0800

    update

--- code/parse_settings.py
+++ code/parse_settings.py
@@ -1,48 +1,72 @@
 import json
 
-with open('../data/WikiHow_settings.json', 'r') as f:
-    setting1 = json.load(f)
-
-with open('../data/WikiHow_settings2.json', 'r') as f:
-    setting2 = json.load(f)
-
-settings = {}
-
-for key, value in setting1.items():
-    settings[key] = value
-
-for key, value in setting2.items():
-    settings[key] = value
-
-save = {}
-cnt = 0
-all_cnt = 0
-for key, value in settings.items():
-    tmp_dic = {}
-    all_flag = 0
-    for v_key, v_value in value.items():
-        value_list = v_value.split('\n')
-        flag = 0
-        for line in value_list:
-            if '[Tools:]' in line:
-                if 'None' in line:
-                    tmp_dic[v_key] = 'None'
-                    flag = 1
-                else:
-                    all_flag = 1
-                    tmp_dic[v_key] = [line.split('[Tools:]')[1].strip(' ')]
-            if flag == 1:
-                break
-            elif '[Scenario Description:]' in line:
-                all_flag = 1
-                tmp_dic[v_key].append(line.split('[Scenario Description:]')[1].strip(' '))
-    if all_flag == 0:
-        cnt += 1
-    all_cnt += len(tmp_dic)
-    save[key] = tmp_dic
-print(len(save))
-print(cnt)
-print(all_cnt)
+# with open('../data/WikiHow_settings.json', 'r') as f:
+#     setting1 = json.load(f)
+#
+# with open('../data/WikiHow_settings2.json', 'r') as f:
+#     setting2 = json.load(f)
+#
+# settings = {}
+#
+# for key, value in setting1.items():
+#     settings[key] = value
+#
+# for key, value in setting2.items():
+#     settings[key] = value
+#
+# save = {}
+# cnt = 0
+# all_cnt = 0
+# for key, value in settings.items():
+#     tmp_dic = {}
+#     all_flag = 0
+#     for v_key, v_value in value.items():
+#         value_list = v_value.split('\n')
+#         flag = 0
+#         for line in value_list:
+#             if '[Tools:]' in line:
+#                 if 'None' in line:
+#                     tmp_dic[v_key] = 'None'
+#                     flag = 1
+#                 else:
+#                     all_flag = 1
+#                     tmp_dic[v_key] = [line.split('[Tools:]')[1].strip(' ')]
+#             if flag == 1:
+#                 break
+#             elif '[Scenario Description:]' in line:
+#                 all_flag = 1
+#                 tmp_dic[v_key].append(line.split('[Scenario Description:]')[1].strip(' '))
+#     if all_flag == 0:
+#         cnt += 1
+#     all_cnt += len(tmp_dic)
+#     save[key] = tmp_dic
+# print(len(save))
+# print(cnt)
+# print(all_cnt)
 
 # with open('../data/WikiHow_setting.json', 'w') as f:
 #     json.dump(save, f, indent=4)
+
+with open('../data/WikiHow_setting.json', 'r') as f:
+    settings = json.load(f)
+
+with open('../data/WikiHow_instructions_rewrite.json', 'r') as f:
+    instructions = json.load(f)
+
+save = {}
+cnt = 0
+for key, value in settings.items():
+    tmp_dic = {}
+    for v_key, v_value in value.items():
+        if v_value != 'None':
+            try:
+                tmp_dic[v_key] = instructions[key][v_key]
+            except:
+                print(key)
+                cnt += 1
+    if len(tmp_dic) != 0:
+        save[key] = tmp_dic
+
+print(cnt)
+with open('../data/WikiHow_filtered_data.json', 'w') as f:
+    json.dump(save, f, indent=4)
---
+++ code/parse_time.py
@@ -0,0 +1,65 @@
+import json
+
+with open('../data/Wikihow_time.json', 'r') as f:
+    times = json.load(f)
+
+cnt = 0
+save = {}
+for key, value in times.items():
+    flag = 0
+    tmp_dic = {}
+    for v_key, v_value in value.items():
+        value_list = v_value.split('\n')
+        if flag == 1:
+            break
+        tmp_list = []
+        for line in value_list:
+            # if 'Step' not in line:
+            #     print(key, v_key)
+            if 'None' in line:
+                # flag = 1
+                cnt += 1
+                break
+            minutes = line.split(':')[1]
+            seconds_string = ''
+            minutes_string = ''
+            if 'minutes' in minutes:
+                minutes_string = minutes.replace('minutes', '')
+            elif 'minute' in minutes:
+                minutes_string = minutes.replace('minute', '')
+            elif 'seconds' in minutes:
+                seconds_string = minutes.replace('seconds', '')
+            else:
+                print(key, v_key, line)
+            if len(seconds_string) == 0:
+                minutes_string = minutes_string.strip(' ')
+                minutes_number = 0
+                try:
+                    minutes_number = int(minutes_string)
+                except:
+                    try:
+                        float(minutes_string)
+                    except ValueError:
+                        print(key, v_key, line)
+                        # print("无法转换为int或float")
+            else:
+                seconds_string = seconds_string.strip(' ')
+                seconds_number = 0
+                try:
+                    seconds_number = int(seconds_string)
+                except:
+                    print(key, v_key, line)
+                minutes_number = seconds_number / 60
+            if (minutes_number > 60):
+                cnt += 1
+                flag = 1
+                break
+            tmp_list.append(minutes_number)
+        tmp_dic[v_key] = tmp_list
+    if flag == 0:
+        save[key] = tmp_dic
+print(cnt)
+print(len(save))
+print(len(times))
+with open('../data/Wikihow_time_appropriate.json', 'w') as f:
+    json.dump(save, f, indent=4)
